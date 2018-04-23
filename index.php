56dfc4562675443173c6dc35b4dc0ceff2508bb5
nanter1986 <nanter1986@gmail.com>
2018-04-23 09:24:00 +0300

    target page

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<form action="confirmUpload.php" method="post" enctype="multipart/form-data">
+		<input type="file" name="theFile">
+		<input type="submit" value="upload" name="submit">
+	</form>
+</body>
+</html>
